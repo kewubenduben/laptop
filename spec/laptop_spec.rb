ff9036dfaa0b39db95429e9313c9bfe4270459d2
Dan Collis-Puro <djcp@thoughtbot.com>
2014-05-16 12:42:03 -0400

    Port existing tests to rspec methods

--- spec/laptop_spec.rb
+++ spec/laptop_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 
 describe 'Laptop applied to a vagrant box' do
+  after(:each) do
+    prepare_for_packaging
+  end
+
   laptop_vagrantfiles.each do |vagrantfile|
     it "should run laptop successfully for #{vagrantfile}" do
+      # build_laptop_script
       link_vagrantfile(vagrantfile)
       destroy_vagrant
       bring_vagrant_up
 
-      expect(laptop_application_to(vagrantfile)).to run_successfully
+      expect { laptop_setup }.not_to raise_error
+
+      expect { zshell_test }.not_to raise_error
+      expect(installed_ruby_version).to eq latest_ruby_version
+      expect { generate_rails_app }.not_to raise_error
+      expect { scaffold_and_model_generation }.not_to raise_error
+      expect { database_migration }.not_to raise_error
+      expect { silver_searcher_test }.not_to raise_error
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'cocaine'
+require 'logger'
+require 'net/http'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
@@ -10,12 +12,20 @@ def laptop_vagrantfiles
   %w|./test/Vagrantfile.debian-jessie-64|
 end
 
+def run_command(command)
+  Cocaine::CommandLine.new(command, '', :logger => Logger.new(STDOUT)).run
+end
+
+def run_vagrant_ssh_command(command)
+  run_command("vagrant ssh -c '#{command}'")
+end
+
 def bring_vagrant_up
-  Cocaine::CommandLine.new('vagrant up').run
+  run_command("vagrant up")
 end
 
 def destroy_vagrant
-  Cocaine::CommandLine.new('vagrant destroy --force').run
+  run_command('vagrant destroy --force')
 end
 
 def link_vagrantfile(vagrantfile)
@@ -27,7 +37,39 @@ def link_vagrantfile(vagrantfile)
   File.symlink(vagrantfile, vagrantfile_name)
 end
 
-def laptop_application_to(vagrantfile)
+def laptop_setup
+  run_vagrant_ssh_command("echo vagrant | bash /vagrant/linux")
+end
 
+def zshell_test
+  run_vagrant_ssh_command('[ "$SHELL" = "/usr/bin/zsh" ]')
+end
 
+def installed_ruby_version
+  run_vagrant_ssh_command("ruby --version")
+end
+
+def latest_ruby_version
+  Net::HTTP.get(URI("http://ruby.thoughtbot.com/latest")).chomp
+end
+
+def generate_rails_app
+  run_vagrant_ssh_command("rm -Rf ~/test_app && cd ~ && rails new test_app")
+end
+
+def scaffold_and_model_generation
+  run_vagrant_ssh_command("cd ~/test_app && rails g scaffold post title:string")
+end
+
+def database_migration
+  run_vagrant_ssh_command("cd ~/test_app && rake db:create db:migrate db:test:prepare")
+end
+
+def silver_searcher_test
+  run_vagrant_ssh_command("command -v ag")
+end
+
+def prepare_for_packaging
+  run_vagrant_ssh_command("rm -Rf ~/test_app")
+  run_vagrant_ssh_command("sudo aptitude clean")
 end
